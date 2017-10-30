6230f7fcf6a53c149600fee56d98d1879cb692c0
vmjfk <vmjfk@kennedylaws.org>
2017-10-29 21:10:41 -0700

    added .c file

--- kennedy_john_programming_assignment_3.c
+++ kennedy_john_programming_assignment_3.c
@@ -31,6 +31,8 @@ typedef struct Vertex3D vertex3d;
 
 vertex3d vertices[10000];
 face faces[10000];
+GLfloat light_diffuse[] = {1.0, 0.0, 0.0, 1.0};  /* Red diffuse light. */
+GLfloat light_position[] = {1.0, 1.0, 1.0, 0.0};  /* Infinite light location. */
 
 GLdouble eyeX, eyeY, eyeZ, refX, refY, refZ, upX, upY, upZ;
 int vert_count;
@@ -101,7 +103,7 @@ void loadWorld()
     printf("loadWorld called \n");
  
     glClear(GL_COLOR_BUFFER_BIT);
-    glColor3f(r,g,b);
+//    glColor3f(r,g,b);
     glPolygonMode(GL_FRONT_AND_BACK,gl_mode);
     glBegin(GL_TRIANGLES);
     printf("loading %d faces\n",face_count);
@@ -120,6 +122,7 @@ void loadWorld()
                 x = v.x;
                 y = v.y;
                 z = v.z;
+                glNormal3f(0.0f,0.0f,1.0f);
                 glVertex3f(x,y,z);
             }
         }
@@ -247,15 +250,24 @@ void init(void)
     printf("init called\n");
     printf("vert_count = %d\n",vert_count);
     glClearColor(0.0,0.0,0.0,0.0);
+  /* Enable a single OpenGL light. */
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHTING);
+
+  /* Use depth buffering for hidden surface elimination. */
+  glEnable(GL_DEPTH_TEST);
+
     //glClearDepth(1.0f);
-    //glEnable(GL_DEPTH_TEST);
+
     //glDepthFunc(GL_LEQUAL);
     glMatrixMode(GL_MODELVIEW);
     eyeX = 0.0f;
-    eyeY = 0.3f;
+    eyeY = 0.0f;
     eyeZ = 1.0f;
     refX = 0.0f; 
-    refY = 0.3f; 
+    refY = 0.0f; 
     refZ = 0.0f; 
     upX = 0.0f; 
     upY = 1.0f; 
